1bce402eef42aa4f4852a48a04e900d2ee1d7d56
pistii <pilisi.istvan@gmail.com>
2025-09-26 17:41:45 +0200

    Implement hero animation

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,7 @@ import Hero from './components/Hero.vue';
 <template>
   <Navbar />
   <Hero />
-  <div class="h-dvh">
 
-  </div>
 </template>
 
 <style scoped>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -33,51 +33,92 @@
         </div>
     </section>
 
+    <div class="video absolute inset-0"> 
+        <video 
+            ref="videoRef"
+            src="/videos/output.mp4" 
+            muted
+            preload="auto"
+        />
+    </div>
 
 </template>
 
 <script>
 import gsap from 'gsap';
 import { SplitText } from 'gsap/all';
-import { onMounted } from 'vue';
+import { onMounted, ref, nextTick } from 'vue';
+
+const isMobile = ref(window.innerWidth <= 767);
+
+
 
 export default {
-    setup() {
-        onMounted(() => {
-            document.fonts.ready.then(() => {
-                const heroSplit = new SplitText(".title", { type: "chars, words" } );
-                const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+    async mounted() {
+        const videoRef = this.$refs.videoRef;
+        await nextTick()
+        console.log(videoRef);
+        const video = videoRef
+        if (!video) {
+            console.error("Video element not found")
+            return
+        }
+        document.fonts.ready.then(() => {
+            const heroSplit = new SplitText(".title", { type: "chars, words" } );
+            const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+            
 
-                heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+            heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
 
-                gsap.from(heroSplit.chars, {
-                    yPercent: 100,
-                    duration: 1.8,
-                    ease: 'expo.out',
-                    stagger: 0.06
-                });
+            gsap.from(heroSplit.chars, {
+                yPercent: 100,
+                duration: 1.8,
+                ease: 'expo.out',
+                stagger: 0.06
+            });
 
-                gsap.from(paragraphSplit.lines, {
-                    opacity: 0,
-                    yPercent: 100,
-                    duration: 1.8,
-                    stagger: 0.06,
-                    delay: 1
-                })
-
-                gsap.timeline({
-                    scrollTrigger: {
-                        trigger: "#hero",
-                        start: 'top top',
-                        end: 'bottom top',
-                        scrub: true
-                    }
-                })
-                .to('.right-leaf', { y: 200 }, 0)
-                .to('.left-leaf', { y: -200 }, 0)
-
+            gsap.from(paragraphSplit.lines, {
+                opacity: 0,
+                yPercent: 100,
+                duration: 1.8,
+                stagger: 0.06,
+                delay: 1
             })
-        });
+
+            gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#hero",
+                    start: 'top top',
+                    end: 'bottom top',
+                    scrub: true
+                }
+            })
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+
+            const startValue = isMobile ? 'top 0%' : 'center 60%';
+            const endValue = isMobile ? '120% top' : 'bottom top';
+
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: 'video',
+                    start: startValue,
+                    end: endValue,
+                    scrub: true,
+                    pin: true,
+                    onEnter: () => video.play(),
+                    onEnterBack: () => video.play(),
+                    onLeave: () => video.pause(),
+                    onLeaveBack: () => video.pause()
+                }
+            })
+
+            video.onloadedmetadata = () => {
+                tl.to(video, {
+                    currentTime: video.duration
+                })
+            }
+        })
     }
 }
 </script>
